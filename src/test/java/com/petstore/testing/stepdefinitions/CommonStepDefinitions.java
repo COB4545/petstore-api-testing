b42d7d4ace80c240e25cc1ad56db4f3831a970fc
Pradeep <pradeepkumar454@gmail.com>
2021-03-23 16:51:40 +0100

    refactored responses

--- src/test/java/com/petstore/testing/stepdefinitions/CommonStepDefinitions.java
+++ src/test/java/com/petstore/testing/stepdefinitions/CommonStepDefinitions.java
@@ -1,15 +1,22 @@
 package com.petstore.testing.stepdefinitions;
 
 import io.cucumber.java.en.Then;
+import org.junit.Assert;
 
-import java.util.Map;
-
+import static net.serenitybdd.rest.SerenityRest.lastResponse;
 import static net.serenitybdd.rest.SerenityRest.restAssuredThat;
-import static org.assertj.core.api.Assertions.assertThat;
 
 public class CommonStepDefinitions {
+
     @Then("status of response is {int}")
     public void status_of_response_is(Integer responseCode) {
         restAssuredThat(response -> response.statusCode(responseCode));
     }
+
+    @Then("the response content is json")
+    public void the_response_content_is_json() {
+        String contentType = lastResponse().getContentType();
+        Assert.assertEquals(contentType,"application/json");
+    }
+
 }
